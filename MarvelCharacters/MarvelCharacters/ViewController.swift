d2fd0ae21017c5434aeea1b3dcae14096c14c74f
Ivo Dutra <ivogdutra@gmail.com>
2020-08-11 14:34:29 -0300

    create proj, gitignore, swiftlint

---
+++ MarvelCharacters/MarvelCharacters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelCharacters
+//
+//  Created by Ivo Dutra on 11/08/20.
+//  Copyright © 2020 Ivo Dutra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
